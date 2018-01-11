34625ce4593148010dc3e288002f050a6a684926
David W.H. Swenson <dwhs@hyperblazer.net>
2018-01-11 15:52:15 +0100

    [codeclim] some refactoring

--- autorelease/release_notes.py
+++ autorelease/release_notes.py
@@ -14,38 +14,61 @@ class ReleaseNoteWriter(GitHubRepoBase):
                 config = yaml.load(f.read())
 
         self.config = config
-        if project is None and 'project' in config.keys():
-            project = ProjectOptions(**config['project'])
-        if github_user is None and 'github_user' in config.keys():
-            github_user = GitHubUser(**config['github_user'])
+        project = self._apply_config_key(project, 'project', ProjectOptions)
+        github_user = self._apply_config_key(github_user, 'github_user',
+                                             GitHubUser)
 
         super(ReleaseNoteWriter, self).__init__(project, github_user)
+        self._latest_release_tag = None
         self._latest_release_tag_name = None
         self._latest_release_commit_date = None
 
-    def label_organized_merged_pulls(self, since=None):
+    def _apply_config_key(self, var, key, obj_cls):
+        # this is to make codeclimate happy
+        if var is None and key in self.config.keys():
+            var = obj_cls(**self.config[key])
+        return var
+
+    def filter_recent_pulls(self, pulls, since):
+        # filter for date
+        pulls = [p for p in pulls if p['merged_at'] > since]
+        # ensure that you don't have the last PR from previous showing up
+        # (there can be a second in time difference between commit and
+        # merge)
+        latest_tag_sha = self._latest_release_tag['commit']['sha']
+        pulls = [p for p in pulls
+                 if p['merge_commit_sha'] != latest_tag_sha]
+        return pulls
+
+    def label_organized_merged_pulls(self, since=''):
         # the implementation challenge is that the return info from pulls
         # doesn't currently include info about labels -- that is contains in
         # the return info from issues (all pulls are issues).
         params = {'state': 'closed'}
-        if since is not None:
+        if since != '':
             params.update({'since': since})
 
         recent_pulls = self.api_get("pulls", params=params).json()
+        recent_pulls = self.filter_recent_pulls(recent_pulls, since)
         recent_issues = self.api_get("issues", params=params).json()
         issues_by_number = {iss['number']: iss for iss in recent_issues}
         desired_pulls = collections.defaultdict(list)
         for pull in recent_pulls:
-            merge_date = pull['merged_at']
-            if merge_date and since and merge_date > since:
-                issue = issues_by_number[pull['number']]
-                label_names = [label['name'] for label in issue['labels']]
-                for label in label_names:
-                    desired_pulls[label] += [pull]
-                if not label_names:
-                    desired_pulls[None] += [pull]
+            issue = issues_by_number[pull['number']]
+            label_names = [label['name'] for label in issue['labels']]
+            for label in label_names:
+                desired_pulls[label] += [pull]
+            if not label_names:
+                desired_pulls[None] += [pull]
 
         return desired_pulls
+
+    def _latest_release_tag_info(self, tag_name):
+        tags = self.api_get("tags").json()
+        desired_tag_list = [t for t in self.api_get("tags").json()
+                            if t['name'] == tag_name]
+        assert len(desired_tag_list) == 1
+        self._latest_release_tag = desired_tag_list[0]
 
     @property
     def latest_release_commit_date(self):
@@ -53,14 +76,12 @@ class ReleaseNoteWriter(GitHubRepoBase):
         # the date of the release itself (can release long after the commit)
         latest_release =  self.api_get("releases/latest").json()
         if self._latest_release_tag_name != latest_release['tag_name']:
-            tags = self.api_get("tags").json()
-            desired_tag_list = [t for t in self.api_get("tags").json()
-                                if t['name'] == latest_release['tag_name']]
-            assert len(desired_tag_list) == 1
-            latest_tag_commit_sha = desired_tag_list[0]['commit']['sha']
+            self._latest_release_tag_info(latest_release['tag_name'])
+            latest_tag_commit_sha = \
+                self._latest_release_tag['commit']['sha']
             commit = self.api_get("commits/" + latest_tag_commit_sha).json()
             self._latest_release_commit_date = \
-                    commit['commit']['committer']['date']
+                     commit['commit']['committer']['date']
             self._latest_release_tag_name = latest_release['tag_name']
         return self._latest_release_commit_date
 
@@ -78,14 +99,19 @@ class ReleaseNoteWriter(GitHubRepoBase):
         out_str += "\n"
         return out_str
 
-    def release_notes_from_pulls(self, pull_dict):
-        out_str = ""
-        label_categories = [lbl['label'] for lbl in self.config['labels']]
-        unknown_labels = set(pull_dict) - set(label_categories)
+    @staticmethod
+    def _pull_to_labels(pull_dict):
         pull_to_labels = collections.defaultdict(list)
         for label in pull_dict:
             for pull in pull_dict[label]:
                 pull_to_labels[pull['number']] += [label]
+        return pull_to_labels
+
+    def release_notes_from_pulls(self, pull_dict):
+        out_str = ""
+        label_categories = [lbl['label'] for lbl in self.config['labels']]
+        unknown_labels = set(pull_dict) - set(label_categories)
+        pull_to_labels = self._pull_to_labels(pull_dict)
 
         treated_pulls = []
 
