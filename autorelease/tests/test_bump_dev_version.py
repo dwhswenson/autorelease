30f08bfae4a91d53f4b855405c513611d7da4351
David W.H. Swenson <dwhs@hyperblazer.net>
2019-12-05 12:21:27 +0100

    Scripts to improve testpypi integration (#45)
    * Start on improvements to testpypi interactions
    
    * No longer require pandoc and RST format (pypi takes md)
    * Will use the bump dev version script to release dev versions
      to testpypi
    * Will use the wait_for_testpypi script to hold testing until version
      is available
    
    * add scripts
    
    * Clean up setup.cfg
    
    Need a release with the scripts installed before I can use them!
    (Unless I want to put in a lot of effort)

---
+++ autorelease/tests/test_bump_dev_version.py
@@ -0,0 +1,24 @@
+import pytest
+from autorelease.scripts.bump_dev_version import *
+
+def test_make_parser():
+    parser = make_parser()
+    opts = parser.parse_args([])
+    assert opts.index == "https://test.pypi.org/pypi"
+    assert opts.conf == "setup.cfg"
+    assert opts.output is None
+
+@pytest.mark.parametrize("v_pypi, v_setup, expected", [
+    ("1.0", "1.1", "1.1"),
+    ("1.0.dev0", "1.0", "1.0.dev0"),
+    ("1.0.rc0", "1.0", "1.0"),
+])
+def test_select_version(v_pypi, v_setup, expected):
+    assert select_version(v_pypi, v_setup) == expected
+
+
+@pytest.mark.parametrize("version_str, expected", [
+    ("1.0", "1.0.dev0"), ("1.0.dev0", "1.0.dev1"),
+])
+def test_bump_dev_version(version_str, expected):
+    assert bump_dev_version(version_str) == expected
