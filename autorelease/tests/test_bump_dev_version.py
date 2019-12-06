23be4f9ce696b55fa74ec6d7ae7495f94c72b827
David W.H. Swenson <dwhs@hyperblazer.net>
2019-12-06 12:20:20 +0100

    Better wait-for-testpypi (finds prereleases)
    fix tests for rename

--- autorelease/tests/test_bump_dev_version.py
+++ autorelease/tests/test_bump_dev_version.py
@@ -1,12 +1,12 @@
 import pytest
 from autorelease.scripts.bump_dev_version import *
 
-def test_make_parser():
-    parser = make_parser()
+def test_shared_parser():
+    parser = shared_parser()
     opts = parser.parse_args([])
     assert opts.index == "https://test.pypi.org/pypi"
     assert opts.conf == "setup.cfg"
-    assert opts.output is None
+    # assert opts.output is None
 
 @pytest.mark.parametrize("v_pypi, v_setup, expected", [
     ("1.0", "1.1", "1.1"),
