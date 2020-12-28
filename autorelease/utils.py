fd19efa9dd2de5b2c019efa428b8c81fdb684930
David W.H. Swenson <dwhs@hyperblazer.net>
2020-12-28 10:02:57 +0100

    import setup in py27, too

--- autorelease/utils.py
+++ autorelease/utils.py
@@ -1,7 +1,7 @@
 import yaml
 import re
 import os
-import importlib.util
+import sys
 
 def conda_recipe_version(recipe_file):
     with open(recipe_file) as f:
@@ -13,8 +13,22 @@ def github_url_to_owner_repo(url):
     match = re.match(pattern, url)
     return match.groups()
 
+
 def import_setup(directory='.'):
     """Return the imported setup.py from the given directory"""
+    if sys.version_info > (3, ):
+        return _import_setup_py3(directory)
+    else:
+        return _import_setup_py2(directory)
+
+
+def _import_setup_py2(directory):
+    import imp
+    return imp.load_source('setup', os.path.join(directory, 'setup.py'))
+
+
+def _import_setup_py3(directory):
+    import importlib.util
     spec = importlib.util.spec_from_file_location(
         'setup', os.path.join(directory, 'setup.py')
     )
