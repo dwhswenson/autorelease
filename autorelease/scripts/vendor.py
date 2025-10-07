d169b68793dd476d5d876ade5595b23ed5eb7288
David W.H. Swenson <dwhs@hyperblazer.net>
2025-10-07 10:57:25 -0400

    Add support for dry runs in `vendor actions`

--- autorelease/scripts/vendor.py
+++ autorelease/scripts/vendor.py
@@ -73,7 +73,8 @@ def get_substitution_mapping(config=None):
     }
 
 
-def vendor(resources, base_path, relative_target_dir, substitutions):
+def vendor(resources, base_path, relative_target_dir, substitutions,
+           dry=False):
     for resource in resources:
         # Use importlib.resources to get the resource content
         try:
@@ -95,14 +96,20 @@ def vendor(resources, base_path, relative_target_dir, substitutions):
         # print(f"cp {resource} {target_loc}")
         template = string.Template(template_content)
 
-        with open(target_loc, mode='w') as wfile:
-            wfile.write(template.substitute(**substitutions))
+        content = template.substitute(**substitutions)
+
+        if dry:
+            print(f"Would write to {target_loc}:")
+            print(content)
+        else:
+            with open(target_loc, mode='w') as wfile:
+                wfile.write(content)
 
 
-def vendor_actions(base_path):
+def vendor_actions(base_path, dry=False):
     resources = ['autorelease-default-env.sh', 'autorelease-prep.yml',
                  'autorelease-gh-rel.yml', 'autorelease-deploy.yml']
     resources = ['gh_actions_stages/' + res for res in resources]
     target_dir = pathlib.Path('.github/workflows')
     substitutions = get_substitution_mapping()
-    vendor(resources, base_path, target_dir, substitutions)
+    vendor(resources, base_path, target_dir, substitutions, dry=dry)
